c7c0fe33b20a663e29ad62445dec6cf9fda34a3a
SiddaramMetri <metrisiddaram@gmail.com>
2023-04-22 10:32:11 +0530

    'updated'

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "../config/axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
---
+++ client/src/config/axios.js
@@ -0,0 +1,5 @@
+import Axios from "axios";
+
+const axios = Axios.create({ baseURL: "http://localhost:4014" });
+
+export default axios;
